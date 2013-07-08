a52b3e8c8081ae88087a6ef3b921b8a1fc49b60a
Thomas Brus <thomas.brus@me.com>
2013-07-08 14:46:47 +0200

    Added params as a serialized field

--- lib/auditing/postgres/request.rb
+++ lib/auditing/postgres/request.rb
@@ -5,34 +5,35 @@ module Auditing
   module Postgres
     class Request < ActiveRecord::Base
       serialize :url_parts, ActiveRecord::Coders::Hstore
+      serialize :params, ActiveRecord::Coders::Hstore
 
-      def url=(url)
-      end
+      # def url=(url)
+      # end
 
-      def modifications
-      end
+      # def modifications
+      # end
 
-      def url_parts=(parts)
-      end
+      # def url_parts=(parts)
+      # end
 
-      def params=(params)
-      end
+      # def params=(params)
+      # end
 
-      def self.find_by_url(url, partial = false)
-      end
+      # def self.find_by_url(url, partial = false)
+      # end
 
-      def self.find_by_url_parts(params = {}, options = {})
-      end
+      # def self.find_by_url_parts(params = {}, options = {})
+      # end
 
-      private
-      def self.collection_name
-      end
+      # private
+      # def self.collection_name
+      # end
 
-      def self.params_to_query_params(hash)
-      end
+      # def self.params_to_query_params(hash)
+      # end
 
-      def url_to_parts(url)
-      end
+      # def url_to_parts(url)
+      # end
     end
   end
 end
